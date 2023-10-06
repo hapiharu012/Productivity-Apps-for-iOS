88f94c63f954b908604c98e4dfc4aa7c34dd9d62
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2023-10-06 17:45:11 +0900

    ウィジェットのデザイン変更

--- Efficio/EfficioWidget/EfficioWidget.swift
+++ Efficio/EfficioWidget/EfficioWidget.swift
@@ -45,9 +45,9 @@ struct EfficioWidgetEntryView : View {
   @Environment(\.managedObjectContext) private var viewContext
   @Environment(\.widgetFamily) var family
   @StateObject private var todoModel = TodoViewModel(context: PersistenceController.shared.container.viewContext)
-//  @FetchRequest( sortDescriptors: [
-//    NSSortDescriptor(keyPath: \Todo.state, ascending: true),
-//  ],animation: .default) private var todos: FetchedResults<Todo>
+  //  @FetchRequest( sortDescriptors: [
+  //    NSSortDescriptor(keyPath: \Todo.state, ascending: true),
+  //  ],animation: .default) private var todos: FetchedResults<Todo>
   
   var filteredTodos: [Todo] {
     print("filter")
@@ -66,225 +66,232 @@ struct EfficioWidgetEntryView : View {
     
     return result
   }
+  
+  //MARK: - BODY
   var body: some View {
+    //MARK: - SMALL
     if family == .systemSmall {
-      VStack {
-        Text("マイタスク")
-          .font(.headline)
-          .fontWeight(.bold)
-        //                        .padding(.top)
-          .position(x:50,y:10)
+      VStack(alignment: .leading){
+          Text("マイタスク")
+            .font(.headline)
+            .fontWeight(.bold)
+            .position(CGPoint(x: 38, y: 5))
+        
+        if !filteredTodos.isEmpty {
+          ForEach(filteredTodos, id: \.self) { todo in
+            HStack {
+              
+              Image(systemName: todo.state ? "checkmark.circle" : "circle")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 15)
+//                .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
+              
+//              Group{
+                Text(todo.name ?? "")
+                  .font(.custom("HelveticaNeue", size: 11))
+                  .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
+                  .lineLimit(1)
+//                  .fixedSize(horizontal: true, vertical: false)
+//                  .background(Color.red)
+
+//              }
+            .foregroundColor(todo.state ? Color.gray : Color.primary)
+                .strikethrough(todo.state)
+              
+//              if (!todo.priority? == "") {
+                Circle()
+                  .frame(width: 6, height: 12, alignment: .center)
+                  .foregroundColor(self.colorize(priority: todo.priority ?? "中"))
+//              }
+            } //END: HSTACK
+            
+          } // END: FOREACH
+//          .position(CGPoint(x: 43.0, y: 8.0))
+        }else {
+          EmptyWidget()
+            .position(CGPoint(x: 55, y: 50))
+        }
+      }//END: VSTACK
+          .widgetBackground(Color.white)
+      }
+    //MARK: - MEDIUM
+      else if family == .systemMedium {
         HStack {
-          Spacer()
-          Text(formatDateTitleDay(Date()))
-            .font(.system(size: 23))
-            .padding(.trailing)
-          VStack(alignment: .leading) {
-            Text(formatDateTitleDayOfWeek(Date()))
-              .font(.system(size: 10))
-            Text(String(SameDayNum(todos: todoModel.todos))+"件")
-              .font(.system(size: 10))
+          VStack(alignment: .leading){
+            Text("マイタスク")
+              .font(.system(size: 24,weight: .bold,design: .default))
+              .padding(.bottom)
+            HStack {
+              //                    Spacer()
+              Text(formatDateTitleDay(Date()))
+                .font(.system(size: 24))
+              //                        .padding(.trailing)
+              VStack(alignment: .leading) {
+                Text(formatDateTitleDayOfWeek(Date()))
+                  .font(.system(size: 11))
+                Text(String(SameDayNum(todos: todoModel.todos))+"件")
+                  .font(.system(size: 11))
+              }
+              //                    Spacer()
+              
+            } // END: HSTACK
           }
-          Spacer()
+          //                Spacer()
           
-        } // END: HSTACK
-        
-        Spacer()
-        
-        ForEach(filteredTodos, id: \.self) { todo in
-          //                if isSameDay(date1: todo.deadline ?? Date(), date2: Date()) {
-          HStack {
-            
-            Image(systemName: todo.state ? "checkmark.circle" : "circle")
-              .resizable()
-              .scaledToFit()
-              .frame(width: 10)
-              .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
-            
-            Group{
-              Text(todo.name ?? "Unknown")
-                .font(.custom("HelveticaNeue", size: 10))
-                .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
-              Spacer()
-              //                            Text(formatDate(todos[index].deadline))
-              //                                .font(.custom("HelveticaNeue", size: 8))
-              //                                .foregroundColor(.gray)
-              
-              
-              Text(todo.priority ?? "Unknown")
-                .font(.custom("HelveticaNeue", size: 8))
-                .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
-            }.foregroundColor(todo.state ? Color.gray : Color.primary)
-              .strikethrough(todo.state)
-            
-            
-          } //END: HSTACK
-          
-          
-          //                }
-        } // END: FOREACH
-        
-        
-      } //END: VSTACK
-      .padding()
-    }
-    else if family == .systemMedium {
-        HStack {
-            VStack(alignment: .leading){
-                Text("マイタスク")
-                    .font(.system(size: 24,weight: .bold,design: .default))
-                    .padding(.bottom)
+          VStack {
+            if !filteredTodos.isEmpty {
+              ForEach(filteredTodos, id: \.self) { todo in
+                //                if isSameDay(date1: todo.deadline ?? Date(), date2: Date()) {
                 HStack {
-                    //                    Spacer()
-                    Text(formatDateTitleDay(Date()))
-                        .font(.system(size: 24))
-                    //                        .padding(.trailing)
-                    VStack(alignment: .leading) {
-                        Text(formatDateTitleDayOfWeek(Date()))
-                            .font(.system(size: 11))
-                      Text(String(SameDayNum(todos: todoModel.todos))+"件")
-                            .font(.system(size: 11))
-                    }
-                    //                    Spacer()
+                  
+                  Image(systemName: todo.state ? "checkmark.circle" : "circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 12)
+                    .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
+                  
+                  
+                  Group{
+                    Text(todo.name ?? "Unknown")
+                      .font(.custom("HelveticaNeue", size: 14))
+                      .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
+                    Spacer()
                     
-                } // END: HSTACK
+                    Text(formatDate(todo.deadline))
+                      .font(.custom("HelveticaNeue", size: 10))
+                      .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
+                      .opacity(0.5)
+                    
+                    
+                    
+                    Circle()
+                      .frame(width: 5, height: 12, alignment: .center)
+                      .foregroundColor(self.colorize(priority: todo.priority ?? "中"))
+                  }.foregroundColor(todo.state ? Color.gray : Color.primary)
+                    .strikethrough(todo.state)
+                  
+                  
+                } //END: HSTACK
+                
+              }// END: FOREACH
+            }else {
+              EmptyWidget()
             }
-            //                Spacer()
-            
-            VStack {
-                ForEach(filteredTodos, id: \.self) { todo in
-                    //                if isSameDay(date1: todo.deadline ?? Date(), date2: Date()) {
-                        HStack {
-                            
-                            Image(systemName: todo.state ? "checkmark.circle" : "circle")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 12)
-                                .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
-                                
-                            
-                            Group{
-                                Text(todo.name ?? "Unknown")
-                                    .font(.custom("HelveticaNeue", size: 14))
-                                    .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
-                                Spacer()
-                                
-                                Text(formatDate(todo.deadline))
-                                    .font(.custom("HelveticaNeue", size: 10))
-                                    .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
-                                    .opacity(0.5)
-                                    
-                                
-                                
-                                Text(todo.priority ?? "Unknown")
-                                    .font(.custom("HelveticaNeue", size: 12))
-                                    .foregroundColor(determiningPriority(priority: todo.priority!) ? .red : .black)
-                            }.foregroundColor(todo.state ? Color.gray : Color.primary)
-                                .strikethrough(todo.state)
-                            
-                            
-                        } //END: HSTACK
-                    
-                }
-            } // END: FOREACH
-            
-            
+          }
+          
+          
         } //END: VSTACK
         .padding(.horizontal)
         .widgetBackground(Color.white)
+      }
+      
     }
-  }
-  func toggleState(for todo: Todo) {
-    todo.state.toggle()
-    do {
-      try viewContext.save()
-    } catch {
-      print(error)
-    }
-  }
-  
-  private func isSameDay(date1: Date, date2: Date) -> Bool {
-    let calendar = SwiftUI.Calendar.current
-    return calendar.isDate(date1, inSameDayAs: date2)
-  }
-  private func SameDayNum(todos: [Todo]) -> Int {
-    let calendar = SwiftUI.Calendar.current
-    var count = 0
-    for todo in todos { // ForEachの代わりに通常のforループを使用
-      if calendar.isDate(todo.deadline ?? Date(), inSameDayAs: Date()) {
-        count += 1
+    func toggleState(for todo: Todo) {
+      todo.state.toggle()
+      do {
+        try viewContext.save()
+      } catch {
+        print("togglrstate - " )
+        print(error)
+        
       }
     }
-    return count
-  }
-  private func formatDateTitleDayOfWeek(_ date: Date?) -> String {
-    guard let date = date else { return "" }
-    let formatter = DateFormatter()
-    formatter.locale = Locale(identifier: "ja_JP")
-    formatter.dateFormat = "EE曜日"
-    return formatter.string(from: date)
-  }
-  private func formatDateTitleDay(_ date: Date?) -> String {
-    guard let date = date else { return "" }
-    let formatter = DateFormatter()
     
-    formatter.dateFormat = "dd日"
-    return formatter.string(from: date)
-  }
-  private func formatDate(_ date: Date?) -> String {
-    guard let date = date else { return "" }
-    let formatter = DateFormatter()
-    formatter.dateFormat = "MM月dd日"
-    return formatter.string(from: date)
-  }
-  private func determiningPriority (priority: String) -> Bool {
+    private func isSameDay(date1: Date, date2: Date) -> Bool {
+      let calendar = SwiftUI.Calendar.current
+      return calendar.isDate(date1, inSameDayAs: date2)
+    }
+    private func SameDayNum(todos: [Todo]) -> Int {
+      let calendar = SwiftUI.Calendar.current
+      var count = 0
+      for todo in todos { // ForEachの代わりに通常のforループを使用
+        if calendar.isDate(todo.deadline ?? Date(), inSameDayAs: Date()) {
+          count += 1
+        }
+      }
+      return count
+    }
+    private func formatDateTitleDayOfWeek(_ date: Date?) -> String {
+      guard let date = date else { return "" }
+      let formatter = DateFormatter()
+      formatter.locale = Locale(identifier: "ja_JP")
+      formatter.dateFormat = "EE曜日"
+      return formatter.string(from: date)
+    }
+    private func formatDateTitleDay(_ date: Date?) -> String {
+      guard let date = date else { return "" }
+      let formatter = DateFormatter()
+      
+      formatter.dateFormat = "d日"
+      return formatter.string(from: date)
+    }
+    private func formatDate(_ date: Date?) -> String {
+      guard let date = date else { return "" }
+      let formatter = DateFormatter()
+      formatter.dateFormat = "MM月dd日"
+      return formatter.string(from: date)
+    }
+    private func determiningPriority (priority: String) -> Bool {
+      switch priority {
+      case "高":
+        return true
+      default:
+        return false
+      }
+    }
+  private func colorize(priority: String) -> Color {
     switch priority {
     case "高":
-      return true
+      return .pink
+    case "中":
+      return .green
+    case "低":
+      return .blue
     default:
-      return false
+      return .clear
     }
   }
-}
-
-struct EfficioWidget: Widget {
-  let persistenceController = PersistenceController.shared
-  let kind: String = "EfficioWidget"
+  }
   
-  var body: some WidgetConfiguration {
-         StaticConfiguration(kind: kind, provider: Provider()) { _ in
-             EfficioWidgetEntryView()
-             .environment(\.managedObjectContext, persistenceController.container.viewContext)
-         }
-         .configurationDisplayName("My Widget")
-         .description("This is an example widget.")
-     }
-}
-
-
-struct EfficioWidget_Previews: PreviewProvider {
+  struct EfficioWidget: Widget {
+    let persistenceController = PersistenceController.shared
+    let kind: String = "EfficioWidget"
+    
+    var body: some WidgetConfiguration {
+      StaticConfiguration(kind: kind, provider: Provider()) { _ in
+        EfficioWidgetEntryView()
+          .environment(\.managedObjectContext, persistenceController.container.viewContext)
+      }
+      .configurationDisplayName("My Widget")
+      .description("This is an example widget.")
+    }
+  }
+  
+  
+  struct EfficioWidget_Previews: PreviewProvider {
     static var previews: some View {
-//        let context = PersistenceController.preview.container.viewContext
-        return EfficioWidgetEntryView()
-            .previewContext(WidgetPreviewContext(family: .systemMedium))
+      //        let context = PersistenceController.preview.container.viewContext
+      return EfficioWidgetEntryView()
+        .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
-}
-
-//#Preview(as: .systemSmall) {
-//  EfficioWidget()
-//} timeline: {
-//  SimpleEntry(date: .now, emoji: "😀")
-//  SimpleEntry(date: .now, emoji: "🤩")
-//}
-
-extension View {
+  }
+  
+  //#Preview(as: .systemSmall) {
+  //  EfficioWidget()
+  //} timeline: {
+  //  SimpleEntry(date: .now, emoji: "😀")
+  //  SimpleEntry(date: .now, emoji: "🤩")
+  //}
+  
+  extension View {
     func widgetBackground(_ backgroundView: some View) -> some View {
-        if #available(iOSApplicationExtension 17.0, *) {
-            return containerBackground(for: .widget) {
-                backgroundView
-            }
-        } else {
-            return background(backgroundView)
+      if #available(iOSApplicationExtension 17.0, *) {
+        return containerBackground(for: .widget) {
+          backgroundView
         }
+      } else {
+        return background(backgroundView)
+      }
     }
-}
+  }
